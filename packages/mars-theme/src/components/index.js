6f678df507d755dc259a7bf3215a7713f4500be3
Imran Sayed <imranhsayed@gmail.com>
2019-09-22 17:05:20 +0530

    first commit

---
+++ packages/mars-theme/src/components/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Global, css, connect, styled, Head } from "frontity";
+import Header from "./header";
+import List from "./list";
+import Post from "./post";
+import Page404 from "./page404.js";
+import Loading from "./loading";
+import Title from "./title";
+
+const globalStyles = css`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      "Droid Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+  a,
+  a:visited {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+const Theme = ({ state }) => {
+  const data = state.source.get(state.router.link);
+
+  return (
+    <>
+      <Head>
+        <meta name="description" content={state.frontity.description} />
+        <html lang="en" />
+      </Head>
+      <Title />
+      <Global styles={globalStyles} />
+      <HeadContainer>
+        <Header />
+      </HeadContainer>
+      <Body>
+        {data.isFetching && <Loading />}
+        {data.isArchive && <List />}
+        {data.isPostType && <Post />}
+        {data.is404 && <Page404 />}
+      </Body>
+    </>
+  );
+};
+
+export default connect(Theme);
+
+const HeadContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #1f38c5;
+`;
+
+const Body = styled.div`
+  display: flex;
+  justify-content: center;
+  background-image: linear-gradient(
+    180deg,
+    rgba(66, 174, 228, 0.1),
+    rgba(66, 174, 228, 0)
+  );
+`;
